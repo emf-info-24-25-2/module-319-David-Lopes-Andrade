45bf57c5b22e1f061652a61da3a5a42eb27aea4a
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-08-29 11:36:07 +0200

    Mon exercice TypeDeBases

---
+++ exercices/exercice03/TypeDeBase.java
@@ -0,0 +1,12 @@
+package exercices.exercice03;
+
+public class TypeDeBase { public static void main(String[] args) {
+    int Age =25;
+    String Prenom ="David";
+    System.out.println("je m'apelle " +Prenom+  " j'ai " +Age + " anées" );
+    System.out.println("Je m'apelle " +Prenom+ " dans 10 ans j'aurais " + (Age + 10) +" anées " ) ;
+
+}
+    
+    
+}
