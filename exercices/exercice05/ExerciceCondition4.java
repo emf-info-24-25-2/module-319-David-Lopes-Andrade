ca2559247b405c7acfeaf465e121ab4424dddace
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-05 10:12:41 +0200

    commit

--- exercices/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/ExerciceCondition4.java
@@ -2,7 +2,7 @@ package exercices.exercice05;
 
 public class ExerciceCondition4 {
     public static void main(String[] args) {
-    int temperature=45;
+    int temperature=89;
 if (temperature < -10) System.out.println("Il fait trés froid");
 else if (temperature <= -10) System.out.println("il fait froid");
 else if(0 <= temperature && temperature <= 25) System.out.println("il fait normal");
