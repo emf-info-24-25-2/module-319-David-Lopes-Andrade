b7469dfcbb254a2e68739cac7960e39a4b5e8df0
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-05 09:20:55 +0200

    commit

--- exercices/exercice05/Exercicecondition2.java
+++ exercices/exercice05/Exercicecondition2.java
@@ -1,5 +1,11 @@
 package exercices.exercice05;
 
 public class Exercicecondition2 {
-    
-}
+    public static void main(String[] args) {
+        int valeurATester = 8;
+        if (valeurATester % 2 == 0){
+            System.out.println("la valeur est paire");
+           }   else {System.out.println("la valeur est impaire");}
+        }
+    }
+
