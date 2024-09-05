002120293e24b069106efcef75ffae00403412e0
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-05 09:46:14 +0200

    modified:   exercices/exercice05/ExerciceCondition4.java

--- exercices/exercice05/ExerciceCondition3.java
+++ exercices/exercice05/ExerciceCondition3.java
@@ -1,5 +1,12 @@
 package exercices.exercice05;
 
 public class ExerciceCondition3 {
-    
+    public static void main(String[] args) {
+        int valeur1 = 6;
+        int valeur2 = 91;
+        int somme = valeur1 + valeur2;
+        if (somme % 2 == 0){
+            System.out.println("le résultat est paire");
+           }   else {System.out.println("le résultat est impaire");}
+    }
 }
--- exercices/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/ExerciceCondition4.java
@@ -1,5 +1,13 @@
 package exercices.exercice05;
 
 public class ExerciceCondition4 {
+    public static void main(String[] args) {
+    int temperature=45;
+if (temperature < -10) System.out.println("Il fait trés froid");
+else if (temperature <= -10) System.out.println("il fait froid");
+else if(0 <= temperature && temperature <= 25) System.out.println("il fait normal");
+else if(25<= temperature && temperature >=35) System.out.println("il fait trés chaud");
+
+}
     
 }
