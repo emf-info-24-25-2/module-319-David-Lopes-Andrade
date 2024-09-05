89c4f2f49acc49431d264403fc95e8c4dbe120de
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-05 09:16:19 +0200

    commit
     devoirs/devoir01/devoir01.java
    
    	modified:   exercices/exercice05/ExerciceCondition1.java

--- devoirs/devoir01/devoir01.java
+++
@@ -1,9 +0,0 @@
-package devoirs.devoir01;
-
-public class devoir01 {
-    public static void main(String[] args) {
-    int monAge = 25;
-    String estMajeur;
-    if monAge < 20 
-    }
-}
--- exercices/exercice05/ExerciceCondition1.java
+++ exercices/exercice05/ExerciceCondition1.java
@@ -2,12 +2,13 @@ package exercices.exercice05;
 
 public class ExerciceCondition1 {
     public static void main(String[] args) {
-    }
+    
     int valeurATester = 50;
     {if(valeurATester > 0)
         System.out.println("la valeur est positive");
-    else if(valeurATester > 0) {
+    else if(valeurATester < 0) {
         System.out.println("La valeur est negative");
     }
     }
+}
 }
