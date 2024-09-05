b992f1bfeaeb540fe437c13dad752e502e13f49b
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-05 10:20:45 +0200

    commit

---
+++ exercices/exercice06/ExerciceSwitch1.java
@@ -0,0 +1,12 @@
+package exercices.exercice06;
+
+public class ExerciceSwitch1 {
+    public static void main(String[] args) {
+        int noteDuModule = 6;
+        if (noteDuModule <=1) System.out.println("Travail non rendu");
+        else if (noteDuModule <=2) System.out.println("trés insuffisant");
+        else if (noteDuModule <=3) System.out.println("Insuffisant");
+        else if (noteDuModule <=4)System.out.println("suffisant");
+        
+    }
+}
