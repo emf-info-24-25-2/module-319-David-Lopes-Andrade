d1072ed2c0d115aa377180bd4ec8d7c5d8539499
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-12 09:01:20 +0200

    commit

--- exercices/ExempleIfSwitch/Operateurdecalcul.java
+++ exercices/ExempleIfSwitch/Operateurdecalcul.java
@@ -3,8 +3,8 @@ package exercices.ExempleIfSwitch;
 public class Operateurdecalcul {
     public static void main(String[] args) {
         
-        int nombre1 =5;
-        int nombre2 =20;
+        int nombre1 =30;
+        int nombre2 =17;
         int nombre3 =41;
         System.out.println("le résultat est " +(nombre1+nombre2));
 
@@ -15,7 +15,7 @@ public class Operateurdecalcul {
         System.out.println("le résultat de multiplication est "+(nombre1*nombre2));
 
 
-        System.out.println("le résultat de la division est "+ (nombre3/nombre1));
+        System.out.println("le résultat de la division est "+ (24.00/51.00));
 
 
         System.out.println("le modulo est "+(nombre1%nombre2));
---
+++ exercices/exercice06/ExerciceSwitch2.java
@@ -0,0 +1,39 @@
+package exercices.exercice06;
+
+public class ExerciceSwitch2 {
+    public static void main(String[] args) {
+    int age=15;
+        
+        switch (age) { 
+            case 7:
+            System.out.println("Poussin");
+              break;  
+        
+        case 8:
+        System.out.println("Pupille");                    
+                    break;
+        
+        case 9:
+        System.out.println("Pupille");
+                    
+        case 10:
+        System.out.println("Minime");
+        case 11:
+        System.out.println("Pupille");
+
+        System.out.println("inconnu");        
+                        break;
+
+          
+
+                }
+                
+                   
+                        
+                }
+    }
+    
+
+
+    
+
