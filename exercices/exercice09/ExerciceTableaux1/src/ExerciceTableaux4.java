f57921e7283dd54f823e40028d4553746ac8494c
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-19 09:32:50 +0200

    commit

--- exercices/exercice09/ExerciceTableaux1/src/ExerciceTableaux4.java
+++ exercices/exercice09/ExerciceTableaux1/src/ExerciceTableaux4.java
@@ -1,11 +1,20 @@
-import java.lang.constant.Constable;
-
 public class ExerciceTableaux4 {
     public static void main(String[] args) {
-        Constable  CAPACITE_RESERVPOIR_A;
-        Constable  CAPACITE_RESERVPOIR_B;
-        int reservoirA ;
-        int reservoirB ;
 
+        int[] MonTableaux = new int [5];
+        MonTableaux[0] = 17;
+
+    for (int i= 1; i <MonTableaux.length;i++) {
+        MonTableaux[i] = MonTableaux[i - 1] + 1;
+
+        System.out.println(MonTableaux[i]);
+    
+    }
+
+    
+       
+        
+       
     }
 }
+
