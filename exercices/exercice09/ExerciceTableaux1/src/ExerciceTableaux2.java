7178b923c5968fda2bfc1cf30a59aab2033d475a
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-19 10:34:12 +0200

    commit

--- exercices/exercice09/ExerciceTableaux1/src/ExerciceTableaux2.java
+++ exercices/exercice09/ExerciceTableaux1/src/ExerciceTableaux2.java
@@ -1,13 +1,28 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class ExerciceTableaux2 {
+    public static final int MAX = 6;
+    public static final int MIN = 1;
     public static void main(String[] args) {
-        int[] MonTableaux = new int[] {1,5,5,4,6,3,1,5,6,4};
+    
+        int total=0;
+
+    Random rand = new Random();
+
+        int[] MonTableaux = new int[10];
+
+        for (int i = 0; i < MonTableaux.length; i++) {
+            MonTableaux[i]=rand.nextInt(MAX-MIN+1)+MIN;
+            total = total + MonTableaux[i];
+        }
         Arrays.toString(MonTableaux);
         for (int i = 0; i < MonTableaux.length; i++) {
-            System.out.println(MonTableaux[i]);
+            System.out.println("cellule "+ i+": " +MonTableaux[i]);
         }
-         
-
     }
 }
+
+
+    
+
