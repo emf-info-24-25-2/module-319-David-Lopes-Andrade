93b00db7deab2b96415bff8bc95bdcac04f73656
Lopes-Andrade-David <david.lopesandrade@studentfr.ch>
2024-09-28 09:11:28 +0200

    commit

--- exercices/exercice10/Methodes.java
+++ exercices/exercice10/Methodes.java
@@ -1,7 +1,20 @@
 package exercices.exercice10;
 
 public class Methodes {
+    public static void math(int name){
+        {
+          
+        }
+    }
+    public static void direBonjour(String name) {
+    
+		System.out.println("Bonjour");
+        }
+        public static void dire(String name) {
+	}
     public static void main(String[] args) {
+        
+		direBonjour(null);
         
         
     }
